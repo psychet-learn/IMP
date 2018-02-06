656002e8cf2bab3aea658411b2d0668f63583fc5
Junseon-Hwang <hwangjs0579@gmail.com>
2018-02-06 10:59:16 +0900

    update

--- hjs/parsing_midi/parsing_midi.py
+++ hjs/parsing_midi/parsing_midi.py
@@ -1,13 +1,8 @@
 import os
 from music21 import converter
-from music21 import midi
-# import matplotlib.pyplot as plt
 
-_full_path = os.path.realpath(__file__)
-_dir_path = os.path.dirname(_full_path)
-
-_train_data_path = _dir_path + '/midi_data/train/'
-_test_data_path = _dir_path + '/midi_data/test/'
+_train_data_path = os.getcwd() + '/midi_data/train/'
+_test_data_path = os.getcwd() + '/midi_data/test/'
 print(_train_data_path)
 # mf = midi.MidiFile()
 # mf.open(_test_data_path + '/adele_-_someone_like_you.mid')
@@ -19,10 +14,11 @@ print(_train_data_path)
 # print(s.flat.elements[20].volume.velocity)
 # eventList = midi.translate.chordToMidiEvents(s)
 
-s = converter.parse(_test_data_path + '/adele_-_someone_like_you.mid') # input midi file directory and parse into stream data
+# '/Users/junseon/Documents/psyche/IMP/hjs/parsing_midi/midi_data/test/adele_-_someone_like_you.mid'
+s = converter.parse(_test_data_path + 'adele_-_someone_like_you.mid') # input midi file directory and parse into stream data
 # s.plot('pianoroll') # print piano roll plot, especially using in jupyter or spyder
 
-s.show('text') # print stream data
+s.show('text', endtime=True) # print stream data
 
 for el in s.recurse(skipSelf=True, streamsOnly=True):
     print(el)
