cbe6b599c1facbf512f1e66db52c0f139e0278df
Junseon-Hwang <hwangjs0579@gmail.com>
2018-02-07 00:41:56 +0900

    parsing data

--- hjs/parsing_midi/parsing_midi.py
+++ hjs/parsing_midi/parsing_midi.py
@@ -37,6 +37,26 @@ oct_list = []
 vel_list = []
 len_list = []
 
+name_to_integer = {'C': 1,
+                   'C#': 2, 'D-': 2,
+                   'D': 3,
+                   'D#': 4, 'E-': 4,
+                   'E': 5,
+                   'F': 6,
+                   'F#': 7, 'G-': 7,
+                   'G': 8,
+                   'G#': 9, 'A-': 9,
+                   'A': 10,
+                   'A#': 11, 'B-': 11,
+                   'B': 12}
+integer_to_name = []
+
+
+def pad(l, maxlen, content=0):
+    l.extend([content] * (maxlen - len(l)))
+    return l
+
+
 for el in s.iter:
     print(str(el))
     for el2 in el.iter:
@@ -48,15 +68,39 @@ for el in s.iter:
                         print("\t\tnote // pitch: " + str(el3.pitch.name) + " / " + str(el3.pitch.octave) +
                               ", vol: " + str(el3.volume.velocity) +
                               ", len: " + str(el3.duration.quarterLength))
+                        pits = pad([name_to_integer[el3.pitch.name]], maxlen=5)
+                        octs = pad([el3.pitch.octave], maxlen=5)
+                        vels = pad([el3.volume.velocity], maxlen=5)
+                        lens = pad([el3.duration.quarterLength], maxlen=5)
+                        pit_list.append(pits)
+                        oct_list.append(octs)
+                        vel_list.append(vels)
+                        len_list.append(lens)
                     else:
                         print("\t\tnote // len:" + str(el3.duration.quarterLength))
                 elif el3.isRest:
                     print("\t\trest // pitch: " + str(el3.pitch) + ", ")
                 elif el3.isChord:
+                    pits_temp = []
+                    octs_temp = []
+                    vels_temp = []
+                    lens_temp = []
                     for el3_1 in el3.pitches:
                         print("\t\tchord // pitch: " + str(el3_1.name) + " / " + str(el3_1.octave) +
                               ", vol: " + str(el3.volume.velocity) +
                               ", len: " + str(el3.duration.quarterLength))
+                        pits_temp.append(name_to_integer[el3_1.name])
+                        octs_temp.append(el3_1.octave)
+                        vels_temp.append(el3.volume.velocity)
+                        lens_temp.append(el3.duration.quarterLength)
+                    pits = pad(pits_temp, maxlen=5)
+                    octs = pad(octs_temp, maxlen=5)
+                    vels = pad(vels_temp, maxlen=5)
+                    lens = pad(lens_temp, maxlen=5)
+                    pit_list.append(pits)
+                    oct_list.append(octs)
+                    vel_list.append(vels)
+                    len_list.append(lens)
                 else:
                     print("\t\t" + str(el3))
         except AttributeError:
@@ -66,3 +110,4 @@ print(pit_list)
 print(oct_list)
 print(vel_list)
 print(len_list)
+
